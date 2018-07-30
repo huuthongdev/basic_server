d7a5bddad67569e4bd52372b50967180e35f957a
huuthongdev <huuthong.mgd@gmail.com>
2018-07-30 17:57:41 +0700

    cm

--- dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./database/connectDatase");
-var app_1 = require("./app");
-app_1.app.listen(process.env.PORT || 4000, function () { return console.log('Server started.'); });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxvQ0FBa0M7QUFDbEMsNkJBQTRCO0FBRTVCLFNBQUcsQ0FBQyxNQUFNLENBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxJQUFJLElBQUksSUFBSSxFQUFFLGNBQU0sT0FBQSxPQUFPLENBQUMsR0FBRyxDQUFDLGlCQUFpQixDQUFDLEVBQTlCLENBQThCLENBQUMsQ0FBQyJ9
